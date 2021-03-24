57beb5afa67c955a8124c6f94050d3a0a0b2d861
danielevergani <dvergani94@gmail.com>
2021-03-24 15:07:38 +0100

    inserito messaggio e immagine

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue ({
+    el: "#root",
+    data: {
+        message: "messaggio di prova",
+        imgSrc: "https://www.tavolartegusto.it/wp/wp-content/uploads/2018/03/pizza-fatta-in-casa-buona-come-in-pizzeria-tutti-i-segreti-Ricetta-Pizza-fatta-in-casa-.jpg",
+        imgAlt: "prova"
+    }
+})
